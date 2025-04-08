da4c62329606a9d8fb5a2ea5ca8104aa7b6ed932
ZORO7861 <prootaku786@gmail.com>
2025-04-08 14:34:42 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -3,67 +3,60 @@ import re
 from importlib import import_module as import_
 
 from pyrogram import filters, idle
-from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
-                            InlineKeyboardMarkup, Message)
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 from pyrogram.enums import ChatType
 
-from spr import BOT_USERNAME, conn, session, spr
+from spr import spr
 from spr.core import ikb
 from spr.modules import MODULES
 from spr.utils.misc import once_a_day, once_a_minute, paginate_modules
+import spr.vars as vars  # importing our variable file
+from spr.core.boot import conn, session  # wherever conn/session is created
 
 HELPABLE = {}
 
-
 async def main():
     await spr.start()
-    # Load all the modules.
+    me = await spr.get_me()
+    vars.BOT_USERNAME = me.username
+    vars.conn = conn
+    vars.session = session
+
+    # Load all the modules
     for module in MODULES:
         imported_module = import_(module)
-        if (
-            hasattr(imported_module, "__MODULE__")
-            and imported_module.__MODULE__
-        ):
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             imported_module.__MODULE__ = imported_module.__MODULE__
-            if (
-                hasattr(imported_module, "__HELP__")
-                and imported_module.__HELP__
-            ):
-                HELPABLE[
-                    imported_module.__MODULE__.lower()
-                ] = imported_module
-    print("STARTED !")
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+
+    print("STARTED!")
     loop = asyncio.get_running_loop()
     loop.create_task(once_a_day())
     loop.create_task(once_a_minute())
     await idle()
-    conn.commit()
-    conn.close()
-    await session.close()
+    vars.conn.commit()
+    vars.conn.close()
+    await vars.session.close()
     await spr.stop()
-
 
 @spr.on_message(filters.command(["help", "start"]), group=2)
 async def help_command(_, message: Message):
     if message.chat.type != ChatType.PRIVATE:
-        kb = ikb({"Help": f"https://t.me/{BOT_USERNAME}?start=help"})
+        kb = ikb({"Help": f"https://t.me/{vars.BOT_USERNAME}?start=help"})
         return await message.reply("Pm Me For Help", reply_markup=kb)
-    kb = ikb(
-        {
-            "Help": "bot_commands",
-            "Repo": "https://github.com/TheHamkerCat/SpamProtectionRobot",
-            "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat (for now)": "https://t.me/WBBSupport",
-        }
-    )
+    kb = ikb({
+        "Help": "bot_commands",
+        "Repo": "https://github.com/TheHamkerCat/SpamProtectionRobot",
+        "Add Me To Your Group": f"https://t.me/{vars.BOT_USERNAME}?startgroup=new",
+        "Support Chat (for now)": "https://t.me/WBBSupport",
+    })
     mention = message.from_user.mention
     await message.reply_photo(
         "https://hamker.me/logo_3.png",
-        caption=f"Hi {mention}, I'm SpamProtectionRobot,"
-        + " Choose An Option From Below.",
+        caption=f"Hi {mention}, I'm SpamProtectionRobot, Choose An Option From Below.",
         reply_markup=kb,
     )
-
 
 @spr.on_callback_query(filters.regex("bot_commands"))
 async def commands_callbacc(_, cq: CallbackQuery):
@@ -71,60 +64,36 @@ async def commands_callbacc(_, cq: CallbackQuery):
     await asyncio.gather(
         cq.answer(),
         cq.message.delete(),
-        spr.send_message(
-            cq.message.chat.id,
-            text=text,
-            reply_markup=keyboard,
-        ),
+        spr.send_message(cq.message.chat.id, text=text, reply_markup=keyboard),
     )
-
 
 async def help_parser(name, keyboard=None):
     if not keyboard:
-        keyboard = InlineKeyboardMarkup(
-            paginate_modules(0, HELPABLE, "help")
-        )
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
     return (
-        f"Hello {name}, I'm SpamProtectionRobot, I can protect "
-        + "your group from Spam and NSFW media using "
-        + "machine learning. Choose an option from below.",
+        f"Hello {name}, I'm SpamProtectionRobot, I can protect your group from Spam and NSFW media using machine learning. Choose an option from below.",
         keyboard,
     )
 
-
 @spr.on_callback_query(filters.regex(r"help_(.*?)"))
 async def help_button(client, query: CallbackQuery):
-    mod_match = re.match(r"help_module\((.+?)\)", query.data)
-    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
-    next_match = re.match(r"help_next\((.+?)\)", query.data)
+    mod_match = re.match(r"help_module(.+?)", query.data)
+    prev_match = re.match(r"help_prev(.+?)", query.data)
+    next_match = re.match(r"help_next(.+?)", query.data)
     back_match = re.match(r"help_back", query.data)
     create_match = re.match(r"help_create", query.data)
+
     u = query.from_user.mention
     top_text = (
-        f"Hello {u}, I'm SpamProtectionRobot, I can protect "
-        + "your group from Spam and NSFW media using "
-        + "machine learning. Choose an option from below."
+        f"Hello {u}, I'm SpamProtectionRobot, I can protect your group from Spam and NSFW media using machine learning. Choose an option from below."
     )
+
     if mod_match:
         module = mod_match.group(1)
-        text = (
-            "{} **{}**:\n".format(
-                "Here is the help for", HELPABLE[module].__MODULE__
-            )
-            + HELPABLE[module].__HELP__
-        )
-
+        text = f"Here is the help for **{HELPABLE[module].__MODULE__}**:\n{HELPABLE[module].__HELP__}"
         await query.message.edit(
             text=text,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "back", callback_data="help_back"
-                        )
-                    ]
-                ]
-            ),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("back", callback_data="help_back")]]),
             disable_web_page_preview=True,
         )
 
@@ -132,9 +101,7 @@ async def help_button(client, query: CallbackQuery):
         curr_page = int(prev_match.group(1))
         await query.message.edit(
             text=top_text,
-            reply_markup=InlineKeyboardMarkup(
-                paginate_modules(curr_page - 1, HELPABLE, "help")
-            ),
+            reply_markup=InlineKeyboardMarkup(paginate_modules(curr_page - 1, HELPABLE, "help")),
             disable_web_page_preview=True,
         )
 
@@ -142,36 +109,26 @@ async def help_button(client, query: CallbackQuery):
         next_page = int(next_match.group(1))
         await query.message.edit(
             text=top_text,
-            reply_markup=InlineKeyboardMarkup(
-                paginate_modules(next_page + 1, HELPABLE, "help")
-            ),
+            reply_markup=InlineKeyboardMarkup(paginate_modules(next_page + 1, HELPABLE, "help")),
             disable_web_page_preview=True,
         )
 
     elif back_match:
         await query.message.edit(
             text=top_text,
-            reply_markup=InlineKeyboardMarkup(
-                paginate_modules(0, HELPABLE, "help")
-            ),
+            reply_markup=InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help")),
             disable_web_page_preview=True,
         )
 
     elif create_match:
         text, keyboard = await help_parser(query)
-        await query.message.edit(
-            text=text,
-            reply_markup=keyboard,
-            disable_web_page_preview=True,
-        )
+        await query.message.edit(text=text, reply_markup=keyboard, disable_web_page_preview=True)
 
     return await client.answer_callback_query(query.id)
-
 
 @spr.on_message(filters.command("runs"), group=3)
 async def runs_func(_, message: Message):
     await message.reply("What am i? Rose?")
-
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
