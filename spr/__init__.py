2d6a3deba5e836938ac09fc0dbf140457339fc57
ZORO7861 <prootaku786@gmail.com>
2025-04-08 14:02:51 +0530

    Update __init__.py

--- spr/__init__.py
+++ spr/__init__.py
@@ -1,34 +1,30 @@
-from os.path import exists
-from sqlite3 import connect
-
+import asyncio
 from aiohttp import ClientSession
 from pyrogram import Client
-from Python_ARQ import ARQ
+from sqlite3 import connect
 
-SESSION_NAME = "spr"
-DB_NAME = "db.sqlite3"
-API_ID = "23012237"
-API_HASH = "c90dc14145de6fe292d1f7034861993a"
-ARQ_API_URL = "https://arq.hamker.dev"
+from spr.utils.db import *
+from spr.utils.misc import *
+from spr.utils.functions import *
+from spr.modules import *
+from spr.core.keyboard import *
+from sample_config import *
+from spr.utils.functions import ARQ, ARQ_API_URL, ARQ_API_KEY
+from spr.utils.db import DB_NAME
 
-if exists("config.py"):
-    from config import *
-else:
-    from sample_config import *
+async def create_arq():
+    session = ClientSession()
+    arq = ARQ(ARQ_API_URL, ARQ_API_KEY, session)
+    return arq, session
 
-session = ClientSession()
-
-arq = ARQ(ARQ_API_URL, ARQ_API_KEY, session)
+arq, session = asyncio.run(create_arq())
 
 conn = connect(DB_NAME)
 
 spr = Client(
-    SESSION_NAME,
+    "spr",
     bot_token=BOT_TOKEN,
     api_id=API_ID,
     api_hash=API_HASH,
+    plugins=dict(root="spr/modules"),
 )
-with spr:
-    bot = spr.get_me()
-    BOT_ID = bot.id
-    BOT_USERNAME = bot.username
