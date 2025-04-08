4e7ac7484efbbf4512a80aa36d7cfb7c5b7e49f8
ZORO7861 <prootaku786@gmail.com>
2025-04-08 14:08:42 +0530

    Update __init__.py

--- spr/__init__.py
+++ spr/__init__.py
@@ -1,25 +1,6 @@
-import asyncio
-from aiohttp import ClientSession
 from pyrogram import Client
-from sqlite3 import connect
-
-from spr.utils.db import *
-from spr.utils.misc import *
-from spr.utils.functions import *
-from spr.modules import *
-from spr.core.keyboard import *
 from sample_config import *
-from spr.utils.functions import ARQ, ARQ_API_URL, ARQ_API_KEY
-from spr.utils.db import DB_NAME
-
-async def create_arq():
-    session = ClientSession()
-    arq = ARQ(ARQ_API_URL, ARQ_API_KEY, session)
-    return arq, session
-
-arq, session = asyncio.run(create_arq())
-
-conn = connect(DB_NAME)
+from spr.core.keyboard import *
 
 spr = Client(
     "spr",
