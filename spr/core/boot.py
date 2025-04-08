f6d0535a34526d763a2697371171c996b118c07b
ZORO7861 <prootaku786@gmail.com>
2025-04-08 14:07:02 +0530

    Create boot.py

---
+++ spr/core/boot.py
@@ -0,0 +1,13 @@
+import asyncio
+from aiohttp import ClientSession
+from sqlite3 import connect
+from spr.utils.functions import ARQ, ARQ_API_URL, ARQ_API_KEY
+from spr.utils.db import DB_NAME
+
+async def create_arq():
+    session = ClientSession()
+    arq = ARQ(ARQ_API_URL, ARQ_API_KEY, session)
+    return arq, session
+
+arq, session = asyncio.run(create_arq())
+conn = connect(DB_NAME)
